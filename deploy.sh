40f5c1432312569701b7b988d4e9907a973965bc
Rayed Alrashed <rayed@rayed.com>
2021-05-08 12:59:19 +0300

    Change deploy method, no need to install hugo on server

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-
-# Check for uncommited changes
-git diff-index --quiet HEAD --
-if [ "$?" -ne "0" ]
-then
-		echo "Couldn't deploy; some changes not commited"
-        exit 1
-fi
-
-# Check if changes pushed
-git status | grep "Your branch is ahead" > /dev/null
-if [ "$?" -eq "0" ]
-then
-		echo "Couldn't deploy; commits not pushed to server"
-        exit 1
-fi
-
-ssh rayed.com ./bin/rayed_com_deploy.sh
+HUGO_ENV="production" hugo
+rsync -azv  public rr.sa:rayed.com
--- gen_disqus_map.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-grep -r "wordpress_id" content/posts | \
-sed 's/^content//' | \
-sed 's#.md:wordpress_id: #/,#' | \
-awk -F, ' { printf("https://rayed.com/wordpress/?p=%s,https://rayed.com%s\n", $2, $1) }' > \
-disqus_map.out.csv
